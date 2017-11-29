e1ca8c7fda62f6048dda5e1d3b2f77e41ae78fd1
mattchri <matthew.christensen@stfc.ac.uk>
2017-11-29 17:10:29 +0000

    Add files via upload
    added ECMWF precip product and two methods to 1) output at each time step and 2) average products at the temporal timestep of ECMWF.

--- process_smip.py
+++ process_smip.py
@@ -8,21 +8,30 @@
 #the primary input product footprint of 3 km resolution at nadir.
 #
 #Required Inputs
-#year month day {in order}
+#year month day version and method {in order}
 #
 #Output
-#NetCDF file containing merged products at each timestep
+#NetCDF file containing derived/merged products at each timestep
+#
+#METHOD 1: output products at their local time
+#METHOD 2: average all products to match ECMWF time
 #
 #Example
-#python2.7 -i process_smip.py 2015 3 1 fv1.0
+#python2.7 -i process_smip.py 2015 3 1 fv1.1 1
+#python2.7 -i process_smip.py 2015 3 1 fv1.2 2
 #
 # 02/11/17, MC: upload initial version of the code to the repo
+# 29/11/17, MC: added ECMWF precip product and two methods to
+#               1) output at each time step and 2) average products
+#               at the temporal timestep of ECMWF.
 #---------------------------------------------------------------
 import sys
 import os
 from netCDF4 import Dataset
 from subroutines import *
-
+import numpy.ma as ma
+from scipy.spatial import cKDTree
+import matplotlib.pyplot as plt
 
 #---------------------------------------------------------------
 # input variables read from command line
@@ -30,13 +39,15 @@ from subroutines import *
 year  = int(sys.argv[1])
 month = int(sys.argv[2])
 dom   = int(sys.argv[3])
-version   = sys.argv[4]
+version = sys.argv[4]
+method  = int(sys.argv[5])
 
 #Extract julian day from input time field
 jday = convert_time(year,month,dom)
 doy = jday_to_doy(jday)
 print "Input Time: ",year,month,dom,doy,jday
-
+print "Version :",version
+print "Averaging Method :",method
 
 #---------------------------------------------------------------
 #Paths (JASMIN)
@@ -76,28 +87,29 @@ MatchedFiles = collocate_files(DataProd, dt)
 
 
 # Read Precipitation Data
-TP = read_ecmwf_variable('TP',year,month,dom)
-#import matplotlib.pyplot as plt
+# Change this to all relevant ECMWF products
+TP = read_ecmwf_variable('TP',year,month,dom) #for accumuluated data you must read in 2-12 hour periods
 #plt.imshow(ARR[0,:,:])
 #plt.show()
-
 
 print 'Collocating Products Together'
 #Merge Data - make netCDF file
 
 #Declare netCDF output variables
-fileID = [0    ,  0  ,    0     ,    0     ,      1          ]
-VARS   = ['lat','lon','toa_swup','toa_lwup','IRprecipitation']
-COLL   = [0    ,  0  ,    0     ,    0     ,      1          ]
+fileID   = [-1   ,      1          ]
+INVARS   = ['tp' ,'IRprecipitation']
+COLL     = [2    ,      1          ]
+OUTVARS  = ['prate_ecmwf','prate_imerg']
+scaleF   = [3600.*1000. , 1.0 ]
+maskVal  = [    0.      , 0.0 ]
 
+varName=OUTVARS
+varLong=['ECMWF_Precipitation','IMERG Precipitation']
+varStan=OUTVARS
+varUnit=['mm/hr','mm/hr']
+varFill=[-999., -999.]
 
-varName=VARS
-varLong=['latitude','longitude','top of atmosphere upwards shortwave flux','top of atmosphere upwards longwave flux','IMERG Precipitation']
-varStan=VARS
-varUnit=['degrees','degrees','W/m^2','W/m^2','mm/hr','W/m^2/sr','um','mm/hr']
-varFill=[-999.    , -999.   , -999. , -999. , -999. , -999.    ,-999., -999.]
-
-nVars  = len(VARS)
+nVars  = len(INVARS)
 
 sz = MatchedFiles.shape
 nFileTypes = sz[0]
@@ -111,6 +123,20 @@ latSV = ncfile.variables['lat'][:]
 xDim = (lonSV.shape)[0]
 yDim = (latSV.shape)[1]
 
+
+# Fetch time indices of each ECMWF timestep that match the SEVIRI timestep
+ecmwf_tid = np.zeros(nFiles)
+for i in range(nFiles):
+    #Base files to do the collocation
+    BaseDict     = DataProd[0] #by default zeroth element
+    BaseFileInfo = BaseDict['files']
+    BT     = BaseFileInfo[i]
+    dTMP = (BT['JDAY'])[1] - ((TP['JDAY'])[1])
+    ecmwf_tid[i] = int(np.asarray( np.where(abs(dTMP) == min(abs(dTMP))) ))
+
+
+
+
 #Loop over each SEVIRI 15 min file
 for i in range(nFiles):
     #Base files to do the collocation
@@ -121,20 +147,12 @@ for i in range(nFiles):
     varData = np.empty( [nVars, xDim, yDim ] )    
     #Loop through each variable
     for j in range(nVars):
-        inFile = MatchedFiles[ fileID[j], i ]
-        print(i,j,inFile)
+        print(i,j,INVARS[j])
 
-        #No collocation required - product already on SEVIRI grid
-        if COLL[j] == 0:
-            ncfile = Dataset( inFile, mode='r')
-            inDATA = ncfile.variables[VARS[j]][:]
-        
         #IMERG to SEVIRI collocation
         if COLL[j] == 1:
-            ncfileIM = Dataset( inFile, mode='r')
-            inDATA = ncfileIM.variables[VARS[j]][:]
 
-            saveFile = outpath+'collocation_seviri_ecmwf'
+            saveFile = outpath+'collocation_seviri_imerg'
             #Check if File already exists
             if os.path.isfile(saveFile+'.npy'):
                 imerg = np.load(saveFile+'.npy')
@@ -149,16 +167,69 @@ for i in range(nFiles):
                 np.save(saveFile, IMERG_ID)
                 LONidIM = IMERG_ID['lonid']
                 LATidIM = IMERG_ID['latid']
+ 
+            if method == 1: #instantaneous output
+                inFile = MatchedFiles[ fileID[j], i ]
+                ncfileIM = Dataset( inFile, mode='r')
+                inDATA = np.asarray( ncfileIM.variables[INVARS[j]][:] )
+                
+                #Extract data from IMERG at SEVIRI location
+                inDATA = inDATA[ LONidIM.astype(int), LATidIM.astype(int) ]
+                inDATA = (LONidIM.astype(int) > -999.)*inDATA + (LONidIM.astype(int) == -999.)*(-999.)
+                
 
-            #Extract data from IMERG at SEVIRI location
-            inDATA = (LONidIM.astype(int) > -999.)*inDATA[ LONidIM.astype(int), LATidIM.astype(int) ] + \
-                     (LONidIM.astype(int) == -999.)*(-999.)
+            if method == 2: #select all files that coincide with the ECMWF averaging period
+                imerg_step_id = (np.where(ecmwf_tid == ecmwf_tid[i]))[0]
+                Nsteps = len(imerg_step_id)
+                ALLData = np.zeros( [ Nsteps, len(LONidIM), len(LATidIM) ] )
+                for k in range(Nsteps):
+                    #Read Each IMERG File
+                    inFile = MatchedFiles[ fileID[j], imerg_step_id[k]]
+                    ncfileIM = Dataset( inFile, mode='r')
+                    inDATA = np.asarray(ncfileIM.variables[INVARS[j]][:])
+                    inDATA = (LONidIM.astype(int) > -1.)*inDATA[ LONidIM.astype(int), LATidIM.astype(int) ] + (LONidIM.astype(int) == -1.)*(0.)
+                    ALLData[k,:,:] = inDATA
+                    #plt.imshow(ALLData[k,:,:], vmin=0.0, vmax=5.0)
+                    #plt.show()
+                inDATA = np.mean(ALLData, axis=0)
 
         #ECMWF Collocation Routine
         if COLL[j] == 2:
             #Fetch Precipitation Data Over Day
-            stop
+            saveFile = outpath+'collocation_seviri_ecmwf'
+            #Check if File already exists
+            if os.path.isfile(saveFile+'.npy'):
+                ecmwf = np.load(saveFile+'.npy')
+                LATidEC = ecmwf.item().get('latid')
+                LONidEC = ecmwf.item().get('lonid')
+            else:
+                print('Collocating ECMWF to SEVIRI grid')
+                #Define lat/lon grid
+                lonEC  = TP['lon']+(TP['lon'][1]-TP['lon'][0])/2.
+                lonEC = (lonEC <= 180.)*(lonEC) + (lonEC > 180.)*(lonEC-360.)
+                latEC  = TP['lat']
 
+                #Run collocation subroutine
+                ECMWF_ID = collocate_imerg_seviri(lonEC,latEC,lonSV,latSV) #uses same code as IMERG
+                np.save(saveFile, ECMWF_ID)
+                LATidEC = ECMWF_ID['lonid']
+                LONidEC = ECMWF_ID['latid']
+
+            #Determine time-step in ECMWF from SEVIRI
+            dTIME = (BT['JDAY'])[1]-(TP['JDAY'])[1]
+            dTIMEid = (np.argsort(dTIME))[::-1]
+            inDATA = (TP['data'])[dTIMEid[0],:,:] * scaleF[j]
+
+            #could take two indices and interpolate but will use closest index in time for now
+            ##data = ma.masked_less(aot,0.0)*0.001
+            lonID = (LONidEC.astype(int) > -1.)*LONidEC + (LONidEC.astype(int) <= -1.)*(-1.)
+            latID = (LATidEC.astype(int) > -1.)*LATidEC + (LATidEC.astype(int) <= -1.)*(-1.)
+
+            #Extract data from ECMWF at SEVIRI location
+            inDATA = (lonID.astype(int) > -1)*inDATA[ latID.astype(int), lonID.astype(int) ] + \
+                     (lonID.astype(int) == -1.)*(0.)
+
+        inDATA = (inDATA >= maskVal[j])*inDATA + (inDATA < maskVal[j])*(-999.)
         varData[j,:,:] = inDATA
 
     #----------------------------------------------------
--- subroutines.py
+++ subroutines.py
@@ -144,8 +144,8 @@ def collocate_imerg_seviri(lonIM,latIM,lonSV,latSV):
                 LONidIM[iX,iY] = int(((np.where(abs(lonIM-tmpLon) == min(abs(lonIM-tmpLon))) )[0])[0])
                 LATidIM[iX,iY] = int(((np.where(abs(latIM-tmpLat) == min(abs(latIM-tmpLat))) )[0])[0])
             else:
-                LONidIM[iX,iY] = -999.
-                LATidIM[iX,iY] = -999.                   
+                LONidIM[iX,iY] = -1.
+                LATidIM[iX,iY] = -1.                   
     IMERG_ID = {'latid':LATidIM, 'lonid':LONidIM, 'xdim':xDim, 'ydim':yDim}
     return IMERG_ID
 
@@ -189,14 +189,119 @@ def make_netcdf_file(ncname,vName,vData,vLong,vStan,vUnit,vFill):
 #ARR [accumulated field]
 #-----------------------------------------------------------------
 def read_ecmwf_variable(vname,year,month,dom):
-    path_ecmwf = '/badc/ecmwf-era-interim/data/ga/fs/'+str(year).zfill(4)+'/'+str(month).zfill(2)+'/'+str(dom).zfill(2)+'/' 
-    name = 'TP'
+    path_ecmwf = '/badc/ecmwf-era-interim/data/ga/fs/'+str(year).zfill(4)+'/'+str(month).zfill(2)+'/'+str(dom).zfill(2)+'/'
+    print('fetching: '+path_ecmwf)
     prefix = str(year).zfill(4)+str(month).zfill(2)+str(dom).zfill(2)
     ftimes = ['0003','0006','0009','0012','1203','1206','1209','1212']
     ARR = np.empty( [8,256,512] )
     for i in range(len(ftimes)):
         ecmwf_file = path_ecmwf+'gafs'+prefix+ftimes[i]+'.nc'
-        print(ecmwf_file)
         ncfile = Dataset( ecmwf_file, mode='r')
+        unitStr = ncfile.variables[vname].units
         ARR[i,:,:] = (ncfile.variables[vname][:])[0,0,:,:]
-    return ARR
+
+    #Express quantity as instantaneous
+    tstep    = 3. * 3600. #3 hours converted to seconds
+    aARR     = np.empty( [8,256,512] )
+    aARR[0,:,:] = ( ARR[0,:,:]-0.        ) / tstep
+    aARR[1,:,:] = ( ARR[1,:,:]-ARR[0,:,:]) / tstep
+    aARR[2,:,:] = ( ARR[2,:,:]-ARR[1,:,:]) / tstep
+    aARR[3,:,:] = ( ARR[3,:,:]-ARR[2,:,:]) / tstep
+    aARR[4,:,:] = ( ARR[4,:,:]-0.        ) / tstep
+    aARR[5,:,:] = ( ARR[5,:,:]-ARR[4,:,:]) / tstep
+    aARR[6,:,:] = ( ARR[6,:,:]-ARR[5,:,:]) / tstep
+    aARR[7,:,:] = ( ARR[7,:,:]-ARR[6,:,:]) / tstep
+
+    #Daily means
+    bARR = np.mean(aARR, axis=0)
+    
+    time = convert_time(year,month,dom)
+    print time
+    hour = np.array([3,6,9,12,15,18,21,24])-1.5
+    fday = hour/24.
+    jday = gcal2jd(year,month,dom)
+    jdayOUT = (jday[0], jday[1] + fday)
+    lat = ncfile.variables['latitude'][:]
+    lon = ncfile.variables['longitude'][:]
+    OUT = {'lat':lat, 'lon':lon, 'data':aARR, 'unit':unitStr+'/s', 'daily':bARR, "JDAY" : jdayOUT}
+    return OUT
+
+def process_ecmwf_variables(year,month,dom):
+    #Read Data
+    STRD = read_ecmwf_variable('STRD',year,month,dom)
+    TISR = read_ecmwf_variable('TISR',year,month,dom)
+    TSR = read_ecmwf_variable('TSR',year,month,dom)
+    TTR = read_ecmwf_variable('TTR',year,month,dom)
+    SSRD = read_ecmwf_variable('SSRD',year,month,dom)
+    SSR = read_ecmwf_variable('SSR',year,month,dom)
+    STRD = read_ecmwf_variable('STRD',year,month,dom)
+    STR  = read_ecmwf_variable('STR',year,month,dom)
+    TP = read_ecmwf_variable('TP',year,month,dom)
+    SSHF = read_ecmwf_variable('SSHF',year,month,dom)
+
+    # Calculate Fluxes
+    L = 2.5e6 #latent heat of vaporization at 0C (J/kg)
+    boa_lwdn = STRD['data']
+    boa_lwup = STR['data'] - STRD['data']
+    boa_swdn = SSRD['data']
+    boa_swup = SSR['data'] - SSRD['data']
+    toa_lwup = TTR['data']
+    toa_swdn = TISR['data']
+    toa_swup = TSR['data'] - TISR['data']
+    prate    = TP['data'] * 1000. * 3600. #(m/s * 3600. * 1000mm/m --> mm/hr)
+    SH     = 0. - SSHF['data']  #not sure if this is right!!!!!
+    LP       = L * prate *(1/3600.) #(J/kg * mm/hr * 1kg/m2 * 1hr/3600s --> W/m2)
+    Rtoa = toa_swdn - toa_swup + 0.       - toa_lwup
+    Rs   = boa_swdn - boa_swup + boa_lwdn - boa_lwup
+    Ra   = Rtoa - Rs
+    dFa  = Ra + LP + SH
+    ecmwf_flux_hourly = [ {'data':STRD['lat'], 'name':'lat'},
+                          {'data':STRD['lon'], 'name':'lon'},
+                          {'data':boa_lwdn, 'name':'boa_lwdn'},
+                          {'data':boa_lwup, 'name':'boa_lwup'},
+                          {'data':boa_swdn, 'name':'boa_swdn'},
+                          {'data':boa_swup, 'name':'boa_swup'},
+                          {'data':toa_lwup, 'name':'toa_lwup'},
+                          {'data':toa_swdn, 'name':'toa_swdn'},
+                          {'data':toa_swup, 'name':'toa_swup'},
+                          {'data':SH, 'name':'SH'},
+                          {'data':prate, 'name':'prate'},
+                          {'data':LP, 'name':'LP'},
+                          {'data':Rtoa, 'name':'Rtoa'},
+                          {'data':Rs, 'name':'Rs'},
+                          {'data':Ra, 'name':'Ra'},
+                          {'data':dFa, 'name':'dFa'}]
+
+    boa_lwdn = STRD['daily']
+    boa_lwup = STR['daily'] - STRD['daily']
+    boa_swdn = SSRD['daily']
+    boa_swup = SSR['daily'] - SSRD['daily']
+    toa_lwup = TTR['daily']
+    toa_swdn = TISR['daily']
+    toa_swup = TSR['daily'] - TISR['daily']
+    prate    = TP['daily'] * 1000. * 3600. #(m/s * 3600. * 1000mm/m --> mm/hr)
+    SH     = 0. - SSHF['daily']
+    LP       = L * prate *(1/3600.) #(J/kg * mm/hr * 1kg/m2 * 1hr/3600s --> W/m2)
+    Rtoa = toa_swdn - toa_swup + 0.       - toa_lwup
+    Rs   = boa_swdn - boa_swup + boa_lwdn - boa_lwup
+    Ra   = Rtoa - Rs
+    dFa  = Ra + LP + SH
+    ecmwf_flux_daily = [ {'data':STRD['lat'], 'name':'lat'},
+                          {'data':STRD['lon'], 'name':'lon'},
+                          {'data':boa_lwdn, 'name':'boa_lwdn'},
+                          {'data':boa_lwup, 'name':'boa_lwup'},
+                          {'data':boa_swdn, 'name':'boa_swdn'},
+                          {'data':boa_swup, 'name':'boa_swup'},
+                          {'data':toa_lwup, 'name':'toa_lwup'},
+                          {'data':toa_swdn, 'name':'toa_swdn'},
+                          {'data':toa_swup, 'name':'toa_swup'},
+                          {'data':SH, 'name':'SH'},
+                          {'data':prate, 'name':'prate'},
+                          {'data':LP, 'name':'LP'},
+                          {'data':Rtoa, 'name':'Rtoa'},
+                          {'data':Rs, 'name':'Rs'},
+                          {'data':Ra, 'name':'Ra'},
+                          {'data':dFa, 'name':'dFa'}]
+
+    ecmwf_flux = {'hourly':ecmwf_flux_hourly,'daily':ecmwf_flux_daily}
+    return ecmwf_flux
